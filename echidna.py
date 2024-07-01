8fa4d1c0ef0d8a878bf9f9b8a67b3719c15e118b
makaloni6 <work.honeycomb@gmail.com>
2024-07-01 14:51:50 +0900

    実装追加

--- echidna.py
+++ echidna.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+import os
 class Echidna:
     def __init__(self, driver):
         self.driver = driver
@@ -9,3 +11,20 @@ class Echidna:
     def scrape(self):
 
         raise NotImplementedError
+    
+    def login(self):
+        try:
+            email_input = self.driver.find_element(
+                by=By.ID, value='ap_email')
+            email_input.send_keys(os.environ['MAIL'])
+        except BaseException as e:
+            input()
+
+        try:
+            password_input = self.driver.find_element(
+                by=By.ID, value='ap_password')
+            password_input.send_keys(os.environ['PASS'])
+        except BaseException as e:
+            input()
+
+        self.driver.find_element(by=By.ID, value='signInSubmit').click()
--- scrape_routine.py
+++ scrape_routine.py
@@ -1,7 +1,10 @@
 import os
+import time
 from selenium import webdriver
 from dotenv import load_dotenv
 from echidna import Echidna
+from selenium.webdriver.common.by import By
+
 
     
 class RoutineScraper(Echidna):
@@ -11,22 +14,51 @@ class RoutineScraper(Echidna):
 
     def cancelSubs(self):
         self.driver.get(self.subscriptions_url)
-        subscription_elements = self.driver.find_elements(...)
+
+        if 'signin' in self.driver.current_url:
+            self.login()
         
+
+
+        subscription_elements = self.driver.find_elements(
+            by=By.CLASS_NAME,
+            value='a-section.subscription-card.aok-inline-block.subscription-hover-state-container'
+        )
+        print('要素数:{}'.format(len(subscription_elements)))
+        input()
         for subscription in subscription_elements:
             self.checkOut(subscription)
 
     def checkOut(self, element):
         pass
 
+
+
+    def test(self, product_id: str) -> None:
+        self.driver.get(os.environ['TEMP_URL'].format(product_id))
+        self.driver.quit()
+
+
     def helloworld(self):
         print('hello world')
 
 
+def get_driver_with_profile(dir_path, profile_path,) -> webdriver.Chrome:
+    options = webdriver.ChromeOptions()
+    options.add_argument(f'--user-data-dir={dir_path}')
+    options.add_argument(f'--profile-directory={profile_path}')
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+def main():
+    load_dotenv()
+    driver = get_driver_with_profile(
+        os.environ['PROFILE_DIR'], os.environ['PROFILE_PATH']
+    )
+    RS = RoutineScraper(driver)
+    RS.cancelSubs()
 
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome()
-    load_dotenv()
-    RS = RoutineScraper(driver)
-    RS.helloworld()
+    main()
