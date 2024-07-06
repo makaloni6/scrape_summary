69e1e578773e38b145ff2591e45a4655aa09898b
makaloni6 <work.honeycomb@gmail.com>
2024-07-06 18:33:42 +0900

    親クラスの実装追加

--- echidna.py
+++ echidna.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 import os
+from utils import login_data
+from selenium.webdriver.chrome.options import Options
+import random
+
 class Echidna:
     def __init__(self, driver):
         self.driver = driver
@@ -15,20 +17,21 @@ class Echidna:
     def scrape(self):
 
         raise NotImplementedError
-    
-    def login(self):
+
+
+    def login(self, acc):
         try:
             email_input = self.driver.find_element(
                 by=By.ID, value='ap_email')
-            email_input.send_keys(os.environ['MAIL'])
+            email_input.send_keys(login_data[acc]['ACC_MAIL'])
         except BaseException as e:
-            input()
+            pass
 
         try:
             password_input = self.driver.find_element(
                 by=By.ID, value='ap_password')
-            password_input.send_keys(os.environ['PASS'])
+            password_input.send_keys(login_data[acc]['ACC_PASS'])
         except BaseException as e:
-            input()
+            pass
 
         self.driver.find_element(by=By.ID, value='signInSubmit').click()
